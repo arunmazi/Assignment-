26930bbf029cfe955a8ac58494d635bd396bb05f
arunmazi <arunmazi0786@gmail.com>
2025-07-05 16:10:04 +0530

    Add files via upload

---
+++ Assignment_simple.py
@@ -0,0 +1,12 @@
+a  = input('Enter your first number')
+b = input ('Enter your second number')
+a = float(a)
+b = float(b)
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print('addition: ',add)
+print('subtration: ', sub)
+print('multiplication: ', mul)
+print('division: ', div)
